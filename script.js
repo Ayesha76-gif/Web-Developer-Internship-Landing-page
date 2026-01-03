fc6b0d65b3b6e30d6976529b2ddec0cb5e0ad522
Ayesha76-gif <shaikayeshamuskan30@gmail.com>
2026-01-03 09:24:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToApply() {
+    document.getElementById("apply").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+function openCognifyz(){
+    window.open("https://cognifyz.com", "_blank");
+}
